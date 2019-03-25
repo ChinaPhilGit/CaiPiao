6076184aa9536bf44440b58cd5b787770a0baf62
飞龙 <tai_fl@163.com>
2019-03-25 22:22:32 +0800

    彩票

--- src/main/java/com/phil/colson/CaiPiao/controller/IndexController.java
+++ src/main/java/com/phil/colson/CaiPiao/controller/IndexController.java
@@ -3,10 +3,10 @@ package com.phil.colson.CaiPiao.controller;
 import com.phil.colson.CaiPiao.entity.CaiPiaoEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.text.SimpleDateFormat;
@@ -20,17 +20,21 @@ public class IndexController {
     @Autowired
     private RedisTemplate<String, List<CaiPiaoEntity>> redisTemplate;
 
-    @PostMapping("/index")
-    public ResponseEntity index(Long dateTime) {
+    @GetMapping("/index")
+    public String index(Model model) {
+        Long dateTime = new Date().getTime();
         Assert.notNull(dateTime, "dateTime不能为空");
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
         List<CaiPiaoEntity> result = redisTemplate.opsForValue().get(sdf.format(new Date(dateTime)));
-        if (result.stream().allMatch(n -> n.getBeginTime().before(new Date(dateTime)) && n.getEndTime().after(new Date(dateTime)))) {
+        model.addAttribute("mo", new CaiPiaoEntity());
+        if (result.stream().anyMatch(n -> n.getBeginTime().before(new Date(dateTime)) && n.getEndTime().after(new Date(dateTime)))) {
 
             CaiPiaoEntity caiPiaoEntity = result.stream().filter(n -> n.getBeginTime().before(new Date(dateTime)) && n.getEndTime().after(new Date(dateTime))).findAny().get();
-            return ResponseEntity.ok(caiPiaoEntity);
+
+            model.addAttribute("mo", caiPiaoEntity);
         }
-        return ResponseEntity.ok(null);
+        return "/index";
+//        return ResponseEntity.ok(null);
     }
 
 }
